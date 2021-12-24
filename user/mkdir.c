04cd3aa77a60e41d0a62b3c4631bebd53bcd6281
sadimer <roman.stolyarov@mail.ru>
2021-12-25 00:26:49 +0300

    Add -p option for mkdir

--- user/mkdir.c
+++ user/mkdir.c
@@ -2,12 +2,42 @@
 
 void 
 umain(int argc, char **argv) {
-    if (argc != 2) {
-        printf("usage: mkdir [directory name or path]\n");
+    if (argc != 2 && argc != 3) {
+        printf("usage: mkdir <-p> [directory name or path]\n");
         return;
     }
-    int res = mkdir((const char *)argv[1]);
-    if (res < 0) {
-        printf("error on creation dir with %s: %d\n", argv[0], res);
+    if (argc == 2) {
+		int res = mkdir((const char *)argv[1]);
+		if (res < 0) {
+			printf("error on creation dir with %s: %d\n", argv[0], res);
+		}
+		return;
+	}
+	if (argc == 3 && !strcmp(argv[1], "-p")) {
+		char cur_path[MAXPATHLEN] = {0};
+		char new[MAXPATHLEN] = {0};
+		char tmp[MAXPATHLEN] = {0};
+		if (argv[2][0] != '/') {
+			getcwd(cur_path, MAXPATHLEN);
+			strcat(cur_path, argv[2]);
+		} else {
+			strcat(cur_path, argv[2]);
+		}
+		beauty_path(new, cur_path);
+		skip_doubledots(tmp, new);
+		strcpy(cur_path, tmp);
+		int len = strlen(cur_path);
+		for (int i = 1; i < len; i++) {
+			if (cur_path[i] == '/') {
+				cur_path[i] = '\0';
+				mkdir(cur_path);
+				cur_path[i] = '/';
+			} else if (i == len - 1) {
+				mkdir(cur_path);
+			}
+		}
+	} else {
+		printf("usage: mkdir <-p> [directory name or path]\n");
+        return;
 	}
 }
